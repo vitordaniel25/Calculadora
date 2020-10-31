dba5fc77bead3dafad0857e3ce1ce049289b0a5a
vitordaniel25 <vitordanieldecastrooliveira@gmail.com>
2020-10-31 15:51:32 -0300

    Versão 1.6

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,14 +46,18 @@
             this.btnApagar = new System.Windows.Forms.Button();
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnResultado = new System.Windows.Forms.Button();
-            this.txbTela = new System.Windows.Forms.TextBox();
-            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbTela = new System.Windows.Forms.Label();
+            this.txbResultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn1
             // 
+            this.btn1.FlatAppearance.BorderSize = 3;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn1.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.Location = new System.Drawing.Point(6, 320);
+            this.btn1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn1.Location = new System.Drawing.Point(40, 322);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(100, 100);
             this.btn1.TabIndex = 0;
@@ -63,8 +67,11 @@
             // 
             // btn2
             // 
+            this.btn2.FlatAppearance.BorderSize = 3;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn2.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn2.Location = new System.Drawing.Point(112, 320);
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn2.Location = new System.Drawing.Point(146, 322);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(100, 100);
             this.btn2.TabIndex = 0;
@@ -74,8 +81,11 @@
             // 
             // btn3
             // 
+            this.btn3.FlatAppearance.BorderSize = 3;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn3.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn3.Location = new System.Drawing.Point(218, 320);
+            this.btn3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn3.Location = new System.Drawing.Point(252, 322);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(100, 100);
             this.btn3.TabIndex = 0;
@@ -85,8 +95,11 @@
             // 
             // btn4
             // 
+            this.btn4.FlatAppearance.BorderSize = 3;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn4.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn4.Location = new System.Drawing.Point(6, 426);
+            this.btn4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn4.Location = new System.Drawing.Point(40, 428);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(100, 100);
             this.btn4.TabIndex = 0;
@@ -96,8 +109,11 @@
             // 
             // btn5
             // 
+            this.btn5.FlatAppearance.BorderSize = 3;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn5.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn5.Location = new System.Drawing.Point(112, 426);
+            this.btn5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn5.Location = new System.Drawing.Point(146, 428);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(100, 100);
             this.btn5.TabIndex = 0;
@@ -107,8 +123,11 @@
             // 
             // btn6
             // 
+            this.btn6.FlatAppearance.BorderSize = 3;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn6.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn6.Location = new System.Drawing.Point(218, 426);
+            this.btn6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn6.Location = new System.Drawing.Point(252, 428);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(100, 100);
             this.btn6.TabIndex = 0;
@@ -118,8 +137,11 @@
             // 
             // btn7
             // 
+            this.btn7.FlatAppearance.BorderSize = 3;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn7.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn7.Location = new System.Drawing.Point(6, 532);
+            this.btn7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn7.Location = new System.Drawing.Point(40, 534);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(100, 100);
             this.btn7.TabIndex = 0;
@@ -129,8 +151,11 @@
             // 
             // btn8
             // 
+            this.btn8.FlatAppearance.BorderSize = 3;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn8.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn8.Location = new System.Drawing.Point(112, 532);
+            this.btn8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn8.Location = new System.Drawing.Point(146, 534);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(100, 100);
             this.btn8.TabIndex = 0;
@@ -140,8 +165,11 @@
             // 
             // btn9
             // 
+            this.btn9.FlatAppearance.BorderSize = 3;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn9.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn9.Location = new System.Drawing.Point(218, 532);
+            this.btn9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn9.Location = new System.Drawing.Point(252, 534);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(100, 100);
             this.btn9.TabIndex = 0;
@@ -151,8 +179,11 @@
             // 
             // btnVirgula
             // 
+            this.btnVirgula.FlatAppearance.BorderSize = 3;
+            this.btnVirgula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVirgula.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVirgula.Location = new System.Drawing.Point(218, 638);
+            this.btnVirgula.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVirgula.Location = new System.Drawing.Point(252, 640);
             this.btnVirgula.Name = "btnVirgula";
             this.btnVirgula.Size = new System.Drawing.Size(100, 100);
             this.btnVirgula.TabIndex = 0;
@@ -162,8 +193,11 @@
             // 
             // btnMultiplicar
             // 
+            this.btnMultiplicar.FlatAppearance.BorderSize = 3;
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMultiplicar.Location = new System.Drawing.Point(324, 532);
+            this.btnMultiplicar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMultiplicar.Location = new System.Drawing.Point(358, 534);
             this.btnMultiplicar.Name = "btnMultiplicar";
             this.btnMultiplicar.Size = new System.Drawing.Size(100, 100);
             this.btnMultiplicar.TabIndex = 0;
@@ -173,8 +207,11 @@
             // 
             // btnSubtrair
             // 
+            this.btnSubtrair.FlatAppearance.BorderSize = 3;
+            this.btnSubtrair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSubtrair.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubtrair.Location = new System.Drawing.Point(324, 426);
+            this.btnSubtrair.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSubtrair.Location = new System.Drawing.Point(358, 428);
             this.btnSubtrair.Name = "btnSubtrair";
             this.btnSubtrair.Size = new System.Drawing.Size(100, 100);
             this.btnSubtrair.TabIndex = 0;
@@ -184,8 +221,11 @@
             // 
             // btnSomar
             // 
+            this.btnSomar.FlatAppearance.BorderSize = 3;
+            this.btnSomar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSomar.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSomar.Location = new System.Drawing.Point(324, 320);
+            this.btnSomar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSomar.Location = new System.Drawing.Point(358, 322);
             this.btnSomar.Name = "btnSomar";
             this.btnSomar.Size = new System.Drawing.Size(100, 100);
             this.btnSomar.TabIndex = 0;
@@ -195,8 +235,11 @@
             // 
             // btnDividir
             // 
+            this.btnDividir.FlatAppearance.BorderSize = 3;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDividir.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDividir.Location = new System.Drawing.Point(324, 638);
+            this.btnDividir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDividir.Location = new System.Drawing.Point(358, 640);
             this.btnDividir.Name = "btnDividir";
             this.btnDividir.Size = new System.Drawing.Size(100, 100);
             this.btnDividir.TabIndex = 0;
@@ -206,8 +249,11 @@
             // 
             // btn0
             // 
+            this.btn0.FlatAppearance.BorderSize = 3;
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn0.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn0.Location = new System.Drawing.Point(6, 638);
+            this.btn0.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn0.Location = new System.Drawing.Point(40, 640);
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(206, 100);
             this.btn0.TabIndex = 0;
@@ -217,8 +263,11 @@
             // 
             // btnApagar
             // 
+            this.btnApagar.FlatAppearance.BorderSize = 3;
+            this.btnApagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnApagar.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnApagar.Location = new System.Drawing.Point(430, 320);
+            this.btnApagar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnApagar.Location = new System.Drawing.Point(464, 322);
             this.btnApagar.Name = "btnApagar";
             this.btnApagar.Size = new System.Drawing.Size(100, 100);
             this.btnApagar.TabIndex = 0;
@@ -228,8 +277,11 @@
             // 
             // btnLimpar
             // 
+            this.btnLimpar.FlatAppearance.BorderSize = 3;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(430, 426);
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLimpar.Location = new System.Drawing.Point(464, 428);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(100, 100);
             this.btnLimpar.TabIndex = 0;
@@ -239,8 +291,11 @@
             // 
             // btnResultado
             // 
+            this.btnResultado.FlatAppearance.BorderSize = 3;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnResultado.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnResultado.Location = new System.Drawing.Point(430, 532);
+            this.btnResultado.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnResultado.Location = new System.Drawing.Point(464, 534);
             this.btnResultado.Name = "btnResultado";
             this.btnResultado.Size = new System.Drawing.Size(100, 206);
             this.btnResultado.TabIndex = 0;
@@ -248,30 +303,41 @@
             this.btnResultado.UseVisualStyleBackColor = true;
             this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel2.Location = new System.Drawing.Point(0, 316);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 442);
+            this.panel2.TabIndex = 2;
+            // 
             // txbTela
             // 
-            this.txbTela.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTela.Location = new System.Drawing.Point(6, 12);
-            this.txbTela.Multiline = true;
+            this.txbTela.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.txbTela.ForeColor = System.Drawing.Color.Thistle;
+            this.txbTela.Location = new System.Drawing.Point(64, 17);
             this.txbTela.Name = "txbTela";
-            this.txbTela.Size = new System.Drawing.Size(523, 213);
-            this.txbTela.TabIndex = 1;
+            this.txbTela.Size = new System.Drawing.Size(500, 213);
+            this.txbTela.TabIndex = 3;
+            this.txbTela.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // txbResultado
             // 
-            this.txbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbResultado.Location = new System.Drawing.Point(7, 231);
-            this.txbResultado.Multiline = true;
+            this.txbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 34F);
+            this.txbResultado.ForeColor = System.Drawing.Color.White;
+            this.txbResultado.Location = new System.Drawing.Point(64, 230);
             this.txbResultado.Name = "txbResultado";
-            this.txbResultado.Size = new System.Drawing.Size(523, 83);
-            this.txbResultado.TabIndex = 1;
-            this.txbResultado.Text = "\n";
+            this.txbResultado.Size = new System.Drawing.Size(500, 83);
+            this.txbResultado.TabIndex = 3;
+            this.txbResultado.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 745);
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(600, 755);
+            this.Controls.Add(this.btn1);
             this.Controls.Add(this.txbResultado);
             this.Controls.Add(this.txbTela);
             this.Controls.Add(this.btnMultiplicar);
@@ -291,12 +357,15 @@
             this.Controls.Add(this.btn5);
             this.Controls.Add(this.btn4);
             this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Calculadora";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -320,8 +389,9 @@
         private System.Windows.Forms.Button btnApagar;
         private System.Windows.Forms.Button btnLimpar;
         private System.Windows.Forms.Button btnResultado;
-        private System.Windows.Forms.TextBox txbTela;
-        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label txbTela;
+        private System.Windows.Forms.Label txbResultado;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,10 +17,38 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case (char)10: result(); break;
+                case (char)48: txbTela.Text += "0"; result(); break;
+                case (char)49: txbTela.Text += "1"; result(); break;
+                case (char)50: txbTela.Text += "2"; result(); break;
+                case (char)51: txbTela.Text += "3"; result(); break;
+                case (char)52: txbTela.Text += "4"; result(); break;
+                case (char)53: txbTela.Text += "5"; result(); break;
+                case (char)54: txbTela.Text += "6"; result(); break;
+                case (char)55: txbTela.Text += "7"; result(); break;
+                case (char)56: txbTela.Text += "8"; result(); break;
+                case (char)57: txbTela.Text += "9"; result(); break;
+                case (char)45: if (operador()) { txbTela.Text += "-"; } result(); break;
+                case (char)43: if (operador()) { txbTela.Text += "+"; } result(); break;
+                case (char)47: if (operador()) { txbTela.Text += "/"; } result(); break;
+                case (char)42: if (operador()) { txbTela.Text += "x"; } result(); break;
+                case (char)8: apagar(); result(); break;
+                case (char)61: result(); break;
+                case (char)27: dados.clear(); txbTela.Text = ""; break;
 
+
+            }
+            e.Handled = true;
+
+        }
 
         private void result()
         {
+            btnResultado.Focus();
             txbResultado.Text = dados.gerarDados(txbTela.Text);
             dados.clear();
         }
@@ -134,18 +162,25 @@ namespace Calculadora
         }
 
         private void btnVirgula_Click(object sender, EventArgs e)
-        {           
+        {
             txbTela.Text += ",";
-          
-            
+
+
         }
 
         private bool operador()
         {
             char op = 'a';
             bool operador = true;
-            op = txbTela.Text.Last<char>();
-            if ((op.Equals('+'))|| (op.Equals('/')) || (op.Equals('x')) || (op.Equals('-')))
+            if (txbTela.Text.Length > 0)
+            {
+                op = txbTela.Text.Last<char>();
+                if ((op.Equals('+')) || (op.Equals('/')) || (op.Equals('x')) || (op.Equals('-')))
+                {
+                    operador = false;
+                }
+            }
+            else
             {
                 operador = false;
             }
@@ -154,14 +189,15 @@ namespace Calculadora
 
         private void btnApagar_Click(object sender, EventArgs e)
         {
+            apagar();
+            result();
+        }
+        private void apagar()
+        {
             if (txbTela.Text.Length > 0)
             {
                 txbTela.Text = txbTela.Text.Remove(txbTela.Text.Length - 1);
             }
-            result();
-            
-            
-
         }
     }
 }
